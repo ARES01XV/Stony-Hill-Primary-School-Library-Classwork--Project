35572b74c5ecf925b84815c73a53d00941c5268d
ARES01XV <princewillis47@gmail.com>
2022-05-25 13:59:32 -0500

    styling added. Sessions and book borrowing left

--- routes/admin.js
+++ routes/admin.js
@@ -5,51 +5,51 @@ var conn = require('../lib/db');
 
 // GET admin 
 router.get('/', function(req, res) {
-  conn.query('SELECT * FROM shps_library.requests', (err, rows) => {
+  conn.query('SELECT * FROM shps_library.requests, students WHERE requests.student_id = students.student_id', (err, rows) => {
     if (err) {
-      res.render('admin', { title: 'Admin', requests: '' });
+      res.render('admin', { title: 'Admin', requests: '', students: ''});
     } else {
-      res.render('admin', { title: 'Admin',  requests: rows});
+      res.render('admin', { title: 'Admin',  requests: rows, students: rows});
     }
   });
 });
 
 // delete
-router.get('/delete/:admin_id', function(req, res) {
-  conn.query('DELETE FROM shps_library.requests WHERE admin_id =' + req.body.admin_id, (err, rows) => {
-    if (err) {
-      res.render('admin', { title: 'Admin', requests: '' });
-    } else {
-      res.render('admin', { title: 'Admin',  requests: rows});
-    }
-  });
-});
+// router.get('/delete/:admin_id', function(req, res) {
+//   conn.query('DELETE FROM shps_library.requests WHERE admin_id =' + req.body.admin_id, (err, rows) => {
+//     if (err) {
+//       res.render('admin', { title: 'Admin', requests: '' });
+//     } else {
+//       res.render('admin', { title: 'Admin',  requests: rows});
+//     }
+//   });
+// });
 
 // Get Update EJS
-router.get('/update/:admin_id', function(req, res) {
-  conn.query('SELECT * FROM shps_library.requests', (err, rows) => {
-    if (err) {
-      res.render('admin-edit', { title: 'Admin', requests: '' });
-    } else {
-      res.render('admin-edit', { title: 'Admin',  requests: rows[0]});
-    }
-  });
-});
+// router.get('/update/:admin_id', function(req, res) {
+//   conn.query('SELECT * FROM shps_library.requests', (err, rows) => {
+//     if (err) {
+//       res.render('admin-edit', { title: 'Admin', requests: '' });
+//     } else {
+//       res.render('admin-edit', { title: 'Admin',  requests: rows[0]});
+//     }
+//   });
+// });
 
 // Post update
   
-router.post('/post', (req, res) => {
-  const data = {
-    admin_id: req.body.admin_id,
-    admin_name: req.body.admin_name,
-    admin_password: req.body.admin_password
-  }
+// router.post('/post', (req, res) => {
+//   const data = {
+//     admin_id: req.body.admin_id,
+//     admin_name: req.body.admin_name,
+//     admin_password: req.body.admin_password
+//   }
 
-  conn.query('UPDATE restaurant_ordering_system.admin SET ? WHERE admin_id =' + req.body.admin_id, data, (err, rows) => {
-    if(err) throw err
-      res.redirect('/admin');
-  });
-});
+//   conn.query('UPDATE shps_library.admin SET ? WHERE admin_id =' + req.body.admin_id, data, (err, rows) => {
+//     if(err) throw err
+//       res.redirect('/admin');
+//   });
+// });
 
 
 
--- routes/books.js
+++ routes/books.js
@@ -4,16 +4,15 @@ var router = express.Router();
 var conn = require('../lib/db');
 
 // GET menu 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   conn.query('SELECT * FROM shps_library.books', (err, rows) => {
     if (err) {
-      res.render('books', { title: 'Books', books: ''[0] });
+      res.render('books', { title: 'Books', books: ''});
     } else {
-      res.render('books', { title: 'Books',  books: rows[0]});
+      console.log(rows)
+      res.render('books', { title: 'Books',  books: rows});
     }
   });
 });
-
-
 
 module.exports = router;
